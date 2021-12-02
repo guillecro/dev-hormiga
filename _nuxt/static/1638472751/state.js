0258640f40eff7f3e1994551fb29a8f47d9bdab6
DevHormiga <devhormiga@gmail.com>
2021-12-02 16:19:47 -0300

    9965020

---
+++ _nuxt/static/1638472751/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdev-hormiga\u002F_nuxt\u002Fstatic\u002F1638472751",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fdev-hormiga\u002F",assetsPath:"\u002Fdev-hormiga\u002F_nuxt\u002F",cdnURL:a}}}}(null));
